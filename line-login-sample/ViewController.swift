042155acfedd8895e6c28cd1e1a34735f941638d
honda kouichi <honda.kouichi@actindi.net>
2015-12-11 10:09:05 +0900

    プロジェクトを追加

---
+++ line-login-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  line-login-sample
+//
+//  Created by kouichi honda on 2015/12/11.
+//  Copyright © 2015年 kouichi honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
